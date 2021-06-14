2a2d528ae3d08123f19ce90423e78513e51a1d12
Lucas <lucasbcmoraes@gmail.com>
2021-06-14 19:41:56 -0300

    foram criadas as funcoes de decisao de jogada do jogador e do computador e a que executa as rodadas.

--- script.js
+++ script.js
@@ -4,11 +4,45 @@ function getRandom() {
 
 function computerPlays() {
   let idjogada = getRandom()
+  let cpujogada
   if (idjogada == 1) {
-    return 'Rock'
+    cpujogada = 'Rock'
   } else if (idjogada == 2) {
-    return 'Paper'
+    cpujogada = 'Paper'
   } else {
-    return 'Scissors'
+    cpujogada = 'Scissors'
   }
+  return cpujogada
+}
+let capitalize = (str) =>
+  str[0].toUpperCase() + str.slice(1, str.length).toLowerCase()
+
+function playerSelection() {
+  let jogadaplayer = prompt('ROCK, PAPER OR SCISSORS')
+  return capitalize(jogadaplayer)
+}
+
+function playRound() {
+  let cpu = computerPlays()
+  let player = playerSelection()
+  console.log(cpu)
+  if (cpu == player) {
+    return 'DRAW'
+  } else if (cpu == 'Rock' && player == 'Paper') {
+    return 'VITORIA'
+  } else if (cpu == 'Paper' && player == 'Scissors') {
+    return 'VITORIA'
+  } else if (cpu == 'Scissors' && player == 'Rock') {
+    return 'VITORIA'
+  } else {
+    return 'DERROTA'
+  }
+}
+let i = 0
+
+function game() {
+  do {
+    i++
+    console.log(playRound())
+  } while (i < 5)
 }
